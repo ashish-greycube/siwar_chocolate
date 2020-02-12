b9632292eb8cfbe522403ce6d4d3fc45c5a98530
ashish-greycube <mr.ashish.shah@gmail.com>
2020-02-12 09:55:57 +0530

    include sales_common

--- siwar_chocolate/siwar_chocolate/doctype/client_request_ct/client_request_ct.js
+++ siwar_chocolate/siwar_chocolate/doctype/client_request_ct/client_request_ct.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2020, GreyCube Technologies and contributors
 // For license information, please see license.txt
+{% include 'erpnext/selling/sales_common.js' %}
 
 frappe.ui.form.on('Client Request CT', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+	onload: function(frm) {
+		if (!frm.doc.delivery_date){
+			frm.set_value('delivery_date', frappe.datetime.get_today())
+		}
+		if (frm.doc.delivery_date){
+		frm.set_query('tray_no', function(doc, cdt, cdn) {
+			return {
+				query: "siwar_chocolate.siwar_chocolate.doctype.client_request_ct.client_request_ct.get_available_tray_list",
+				filters: {
+					'delivery_date': doc.delivery_date
+				}
+			}
+		});	
+	}	
+	
+	}
 });
--- siwar_chocolate/siwar_chocolate/doctype/client_request_ct/client_request_ct.py
+++ siwar_chocolate/siwar_chocolate/doctype/client_request_ct/client_request_ct.py
@@ -3,8 +3,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.controllers.selling_controller import SellingController
 
-class ClientRequestCT(Document):
-	pass
+class ClientRequestCT(SellingController):
+	def __init__(self, *args, **kwargs):
+		super(ClientRequestCt, self).__init__(*args, **kwargs)	
+
+def get_available_tray_list(doctype, txt, searchfield, start, page_len, filters):
+
+	default_company = frappe.db.get_single_value('Global Defaults', 'default_company')
+	tray_asset_category_cf = frappe.db.get_value('Company', default_company, 'tray_asset_category_cf')
+	delivery_date=filters.get("delivery_date")
+	
+	print(tray_asset_category_cf,delivery_date)
+	test=frappe.db.sql("""select name from `tabAsset` asset where asset_category =%s and name not in
+(select distinct(tray_no) from `tabClient Request CT` where 
+`tabClient Request CT`.docstatus = 0 and
+`tabClient Request CT`.delivery_date =%s)""",(tray_asset_category_cf,delivery_date))
+	print('test',test)
+	return test
+	
+
+
